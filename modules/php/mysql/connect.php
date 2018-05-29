a3af3ce2c7540e472b7934b6792f348b5ce26768
ImPedro29 <pi3virgula14@outlook.com>
2018-05-29 19:17:39 -0300

    Dados da Database adicionados

--- modules/php/mysql/connect.php
+++ modules/php/mysql/connect.php
@@ -1,3 +1,8 @@
 <?php
 //CONEXÃO COM O BANCO DE DADOS
 //> é daqui que você dá o include
+	$servidor = '35.198.28.56';
+	$usuario = 'getdiat';
+	$senha = '2Sjp60qLwuwflbUf';
+	$banco = 'getdiat';
+	$con = new mysqli($servidor, $usuario, $senha, $banco) or die("Conexão com banco de dados não pode ser estabelecida.");
